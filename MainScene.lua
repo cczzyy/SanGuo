c3115b6edabf74370d09abc7e272088bb31838be
cczzyy <1157178588@qq.com>
2015-09-17 10:11:54 +0800

    dasas
    asda

---
+++ MainScene.lua
@@ -0,0 +1,34 @@
+
+local Player=require("app.scenes.Enemy")
+   
+local MyScene = class("MyScene", function ()  
+    return display.newScene("MyScene")  
+end)  
+   local bone
+function MyScene:ctor()   
+     sprite=ccs.ArmatureDataManager:getInstance():addArmatureFileInfo("zhaoyun.ExportJson")
+     sprite = ccs.Armature:create("zhaoyun")
+     sprite:pos(300,300)
+    
+     self:addChild(sprite)
+
+     local a=sprite:getShapeList()
+     
+
+     print(a:getBoundingBox())
+     local skin1 = ccs. Skin:create("skin/goldenCloak/pifeng7.png")
+
+           -- 获取到装备的骨骼, 并添加新的显示
+     bone = sprite:getBone("披风"):addDisplay(skin1, 1)
+      sprite:getAnimation():play("跑步")
+     transition.moveTo(sprite, {x = sprite:getPositionX()+200, y = sprite:getPositionY(), time =0.1})
+    print(bone:getPositionX())
+       --schedule_collision=scheduler.scheduleUpdateGlobal(handler(self, self.update),0.1)   
+ 
+end  
+   
+   function MyScene:update(dt)
+    transition.moveTo(sprite, {x = sprite:getPositionX()+2, y = sprite:getPositionY(), time =0.1})
+   	print(bone:getPositionX())
+   end
+return MyScene
